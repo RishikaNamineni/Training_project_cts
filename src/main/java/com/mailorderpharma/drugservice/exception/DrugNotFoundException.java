ed645f4f885a377437c57e3748cc40611f184305
RishikaNamineni <79569514+RishikaNamineni@users.noreply.github.com>
2022-07-01 10:52:35 +0530

    Delete src directory

--- src/main/java/com/mailorderpharma/drugservice/exception/DrugNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mailorderpharma.drugservice.exception;
-
-@SuppressWarnings("serial")
-public class DrugNotFoundException extends RuntimeException{
-
-	public DrugNotFoundException(String message) {
-		super(message);
-	}
-}
--- src/main/java/com/mailorderpharma/drugservice/exception/StockNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mailorderpharma.drugservice.exception;
-
-@SuppressWarnings("serial")
-public class StockNotFoundException extends Exception {
-	public StockNotFoundException(String message) {
-		super(message);
-	}
-}
